5266f263be7e1b2ed4a09bc82acb1e39582fec67
nferrari3444 <nferrari3444@gmail.com>
2022-11-21 23:21:47 -0300

    change static path

--- index.js
+++ index.js
@@ -88,19 +88,19 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname,'/client/build')))
+// app.use(express.static(path.join(__dirname,'/client/build')))
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+// app.get('*', (req,res) => {
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
 
 
-// app.get('/', (req, res) => {
+app.get('/', (req, res) => {
 
-//     res.send('hola hola');
-// });
+    res.send('hola hola');
+});
 
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
